c52a9a78780a24a892f367e1f616b0d79992aaf7
TCross4 <144451394+TCross4@users.noreply.github.com>
2025-03-04 17:00:29 +0000

    Update Program.cs

--- RecordsStructureTask1/Program.cs
+++ RecordsStructureTask1/Program.cs
@@ -2,6 +2,38 @@
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            Film hulk = new Film("Hulk", "12A", 2005, false);
+            Film ironMan = new Film("Iron Man","12A",2008,false);
+            Film antMan = new Film("Ant-Man", "12A", 2015, false);
+            Film[] Film = { antMan,hulk,ironMan };
+            int year = 0;
+            int position = 0;
+            string name = "0";
+            
+            year = antMan.year;
+            name = antMan.title;
+            for (int i = 0; i < 3; i++)
+            {
+                if (Film[i].year > year)
+                {
+                    Film[i].title = name;
+                    Film[i].year = year;
+                }
+            }
+            Console.WriteLine($"{name} {year}");
+            
+            antMan.beingShown = true
+
+            //  Write the code to do the following
+            // Loop through the array of films and check for the newest film
+            // Produce one output to say the name of the newest film
+
+            // Write the code to update the andMan record to show the film is currently being shown
+
+
+        }
         struct Film
         {
             public string title;
@@ -17,23 +49,6 @@
                 this.beingShown = beingShown;
 
             }
-        }
-        static void Main(string[] args)
-        {
-            Film hulk = new Film("Hulk", "12A", 2005, false);
-            Film ironMan = new Film("Iron Man","12A",2008,false);
-            Film antMan = new Film("Ant-Man", "12A", 2015, false);
-            Film[] filmCollection = new Film[] { antMan,hulk,ironMan };
-            int year = 0;
-            int position = 0;
-
-            //  Write the code to do the following
-            // Loop through the array of films and check for the newest film
-            // Produce one output to say the name of the newest film
-
-            // Write the code to update the andMan record to show the film is currently being shown
-
-
         }
     }
 }
